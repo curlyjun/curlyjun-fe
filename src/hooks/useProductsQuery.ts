cf38a28b647d91559c319d44b6d7af32f62aec9d
curlyjun <tjdwns4850@gmail.com>
2022-07-10 21:47:57 +0900

    feat: useProductQuery 훅 추가

---
+++ src/hooks/useProductsQuery.ts
@@ -0,0 +1,28 @@
+import { Product } from '#types/product';
+import { convertQueryStringToPositiveNumber } from '@utilities/index';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
+
+interface ProductsResponse {
+  data: {
+    products: Product[];
+    totalCount: number;
+  };
+}
+
+export const fetchProducts = async (page: number, size: number) => {
+  const { data } = await axios.get<ProductsResponse>(`/products?page=${page}&size=${size}`);
+
+  return data.data;
+};
+
+export const useProductsQuery = () => {
+  const { query } = useRouter();
+  const page = convertQueryStringToPositiveNumber(query.page) || 1;
+  const size = convertQueryStringToPositiveNumber(query.size) || 10;
+
+  const queryResult = useQuery('products', () => fetchProducts(page, size));
+
+  return queryResult;
+};
