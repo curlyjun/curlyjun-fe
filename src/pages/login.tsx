f17b2ebdc3841c2507f7cb777254b654db97acac
curlyjun <tjdwns4850@gmail.com>
2022-07-13 15:32:54 +0900

    refactor: 서버사이드에서 유저 검증 후 redirect하도록 롤백

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,24 +1,12 @@
-import Cookies from 'js-cookie';
-import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
+import type { GetServerSideProps, NextPage } from 'next';
 import React from 'react';
-import { useEffect } from 'react';
 
 import { Header } from '@/components/header';
 import { LoginForm } from '@/components/loginForm';
 import * as cookieName from '@/constants/cookies';
-import { useUserQuery } from '@/hooks/queries/useUserQuery';
+import { fetchUser } from '@/hooks/queries/useUserQuery';
 
 const LoginPage: NextPage = () => {
-  const { data: user, isFetched } = useUserQuery(Cookies.get(cookieName.USER_ID));
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isFetched && user) {
-      router.push('/');
-    }
-  }, [user, isFetched, router]);
-
   return (
     <>
       <Header />
@@ -28,3 +16,30 @@ const LoginPage: NextPage = () => {
 };
 
 export default LoginPage;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const userId = req.cookies[cookieName.USER_ID];
+
+  if (userId) {
+    try {
+      /** 유저 검증 후 유저 있으면 redirect */
+      const user = await fetchUser(userId);
+      if (user?.NAME) {
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          },
+        };
+      }
+    } catch (error) {
+      return {
+        props: {},
+      };
+    }
+  }
+
+  return {
+    props: {},
+  };
+};
